556c8745a51eb0f54257a96a92ef4e96337c99f5
serii burduja <seriiburduja@gmail.com>
2025-11-19 17:36:53 +0200

    feat: abstract factory 3

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import singletonLogger from "./modules/43.singletonLogger";
+import abstractPizza from "./practice/abstract-factory/abstract-factory05";
 import calculator from "./practice/calculator";
 import game from "./practice/game";
 import userClone from "./practice/prototype/proto01";
 
+abstractPizza();
 // userClone()
 // singletonLogger()
 // calculator()
